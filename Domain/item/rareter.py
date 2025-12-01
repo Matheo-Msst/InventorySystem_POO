bff4a9a505e1346e159e2b3f488ac08800f5e86c
pvictori <victoria.pasquaud@ynov.com>
2025-12-01 12:11:44 +0100

    mise en place fichier

---
+++ Domain/item/rareter.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Rareter(Enum):
+    COMMUN = 1
+    RARE = 2
+    EPIC = 3
+
+def chance_drop(self):
+   return {
+        self.COMMUN;
+   }
+
+def rareter_couleur(self):
+    return {
+        self.COMMUN("white"),
+        self.RARE("Vert"),
+        self.EPIC("Jaune")
+    }
